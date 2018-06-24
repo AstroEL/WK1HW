0dbd8731b9ceafd2ae5e8a9087b25f5e9cdfc0b8
Leiland <leiland12@gmail.com>
2018-06-23 23:25:57 -0700

    wk1hwLT

---
+++ wk1hwLT.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+/**
+ *
+ */
+package wk1hw;
+public class wk1hwLT {
+// --------------------------- Coding Portion  ---------------------------------// 
+	public static void main(String[] args) {
+			byte numberOfCookies = 13;
+			byte monthsInBeta = 15;
+			short flareTemp = 12000;
+			short peakTemp = 14000;
+			long deptCEO = 130000000;
+			long debtUSA = 654322432;
+			int age = 32;
+			int weight = 160;
+			char MR = 99;
+			char DR = 7;
+			float height = 6;
+			float decimal = 10;
+			double KP = 10.4325;
+			double KPUV = 16.432;
+			boolean male = true;
+			boolean female = false;
+			
+			System.out.println(numberOfCookies);
+			System.out.println(monthsInBeta);
+			System.out.println(flareTemp);
+			System.out.println(peakTemp);
+			System.out.println(deptCEO);
+			System.out.println(debtUSA);
+			System.out.println(age);
+			System.out.println(weight);
+			System.out.println(MR);
+			System.out.println(DR);
+			System.out.println(height);
+			System.out.println(decimal);
+			System.out.println(KP);
+			System.out.println(KPUV);
+			System.out.println(male);
+			System.out.println(female);
+
+// --------------------------- Research Portion ---------------------------------// 			
+/* Git Workflow:
+ * 
+ * Git workflow is essentially the structure and process enforced in collaborative coding
+ * to facilitate accurate and flexible team work in an environment where many small and sensitive 
+ * changes to the behind-the-scenes framework are happening at various times. Git workflow 
+ * structures primarily focus around a main central repository on a system like GitHub
+ * where each user can branch his or her own work to and from the main stem as they pull, commit, 
+ * push and merge and ultimately manage any conflicts that arise as the project develops. 
+ * Each user involved downloads a clone of the central repository to act as a local backup and 
+ * testing ground which ensures added security as well as convenience for making changes. 
+ * 
+ * Sources:
+ * https://www.atlassian.com/git/tutorials/comparing-workflows
+ * https://git-scm.com/book/en/v2/Git-Branching-Branching-Workflows
+ * https://en.wikipedia.org/wiki/Git
+ * 
+ * Data Types in Java:
+ * 
+ * In Java there are two main Data Types namely, primitive data types and object data types.
+ * In general, data types are containers or ways to store information for a compiler to read.
+ * When we refer to primitive data types in Java, we refer to the most basic data types available
+ * in java that contain pure values. Within the primitive data types, there is a division;
+ * 		Numeric primitives: short, int, long, float and double.  These primitive data types 
+ * 			hold only numeric data. Operations associated with such data types are those of 
+ * 			simple arithmetic (addition, subtraction, etc.) or of comparisons (is greater than, 
+ * 			is equal to, etc.)
+ *		Textual primitives: byte and char. These primitive data types hold characters (that 
+ *			can be Unicode alphabets or even numbers). Operations associated with such types are 
+ *			those of textual manipulation (comparing two words, joining characters to make words, 
+ *			etc.). 
+ *		Boolean and null primitives: Boolean is a data type for true and false statements and null
+ *			refers to values that are unset or set to 'nothing'. 
+ * Beyond the primitive data types, we have object data types. These are data types that are 
+ * created to have not only data as values but instructions as to what to do with them. These objects
+ * can be real things like a creature in a game or an object on a map in a race. 
+ * 
+ * 
+ * Sources:
+ * https://www.geeksforgeeks.org/data-types-in-java/
+ * https://www.programiz.com/java-programming/variables-primitive-data-types
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+ * 
+ * 
+ * 
+ * 
+ */
+		}
+		
+		
+}
